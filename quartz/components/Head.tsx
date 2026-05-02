5459f680a89d370f3cd99bc2b6e27f161950eefa
tmpdubz <t.m.dubiel@gmail.com>
2026-05-01 22:44:34 -0400

    Fix CMS branch to v4 and add identity widget login redirect

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -86,6 +86,17 @@ export default (() => {
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          if (window.netlifyIdentity) {
+            window.netlifyIdentity.on("init", function(user) {
+              if (!user) {
+                window.netlifyIdentity.on("login", function() {
+                  document.location.href = "/admin/";
+                });
+              }
+            });
+          }
+        `}} />
 
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
